0e97f3869a485d8517cf24489f48eb0e3ee28f6a
Sagar <sagarjain2104@gmail.com>
2023-09-13 16:21:19 +0530

    registered Permission model in admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from authentication.models import User
+from django.contrib.auth.models import Permission
 
 
 class UserAdmin(BaseUserAdmin):
@@ -61,3 +62,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 # admin.site.register(User)
+admin.site.register(Permission)
