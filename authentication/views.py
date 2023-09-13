69abc728411f19d6bbc73a52a4a95dd6ed248873
Sagar <sagarjain2104@gmail.com>
2023-09-13 16:25:30 +0530

    Added Permissions for Buyer and Seller

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Permission, Group
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect, render
 
@@ -6,61 +7,87 @@ from django.shortcuts import redirect, render
 # from core import settings
 from django.template.loader import render_to_string
 from django.views.generic import TemplateView
-from django.template import Context
 from django.template.loader import get_template
 
 from authentication.forms import CreateUserForm
 from product.models import Product
+from authentication.models import User
 
 # from django.utils.html import strip_tags
 
 
 class RegisterView(TemplateView):
-
     def get(self, request, *args, **kwargs):
-
         if request.user.is_authenticated:
-            return redirect('/home')
+            return redirect("/home")
 
         form = CreateUserForm()
 
         return render(
             request,
-            template_name='authentication/signup.html',
+            template_name="authentication/signup.html",
             context={
                 "signupform": form,
-            }
+            },
         )
 
     def post(self, request, *args, **kwargs):
-
         form = CreateUserForm(request.POST, request.FILES)
         if form.is_valid():
             # new_user = form.save(commit=False)
             # password = form.cleaned_data['password1']
             # new_user.set_password(password)
             # new_user.save()
-            new_user_email = form.cleaned_data['email']
-            user_first_name = form.cleaned_data['first_name']
+            new_user_email = form.cleaned_data["email"]
+            user_first_name = form.cleaned_data["first_name"]
             form.save()
+            created_user = User.objects.get(email=new_user_email)
+            if created_user.type == "SELLER":
+                group = Group.objects.get(name='seller')
+                permissions = Permission.objects.filter(
+                    codename__in=[
+                        "add_product",
+                        "change_product",
+                        "delete_product",
+                        "view_product",
+                        "add_category",
+                        "change_category",
+                        "delete_category",
+                        "view_category",
+                    ]
+                )
+                created_user.user_permissions.add(*permissions)
+                created_user.groups.add(group)
+            else:
+                group = Group.objects.get(name='buyer')
+                permissions = Permission.objects.filter(
+                    codename__in=[
+                        "add_cart",
+                        "change_cart",
+                        "delete_cart",
+                        "view_cart",
+                        "view_product",
+                    ]
+                )
+                created_user.user_permissions.add(*permissions)
+                created_user.groups.add(group)
             # html_template = 'authentication/signup_email.html'
             # html_message = render_to_string(html_template)
             # plain_message = strip_tags(html_message)   #method 2
             context = {"user_first_name": user_first_name}
-            message = get_template("authentication/signup_email.html").render(context)
+            message = get_template("authentication/signup_email.html").render(
+                context
+            )
 
             def send_email(
-                    to_list,
-                    subject,
-                    message,
-                    sender='jainsagar619@gmail.com'
-                    ):
+                to_list, subject, message, sender="jainsagar619@gmail.com"
+            ):
                 msg = EmailMessage(subject, message, sender, to_list)
                 msg.content_subtype = "html"
                 return msg.send()
 
             send_email(
-                subject='Registration Successful',
+                subject="Registration Successful",
                 message=message,
                 to_list=[new_user_email],
             )
@@ -71,123 +98,113 @@ class RegisterView(TemplateView):
             #     from_email='jainsagar619@gmail.com',
             #     recipient_list=[new_user_email]
             # )
-            return redirect('/login')
+            return redirect("/login")
 
         return render(
             request,
-            template_name='authentication/signup.html',
+            template_name="authentication/signup.html",
             context={
                 "signupform": form,
-            }
+            },
         )
 
 
 class LoginView(TemplateView):
-
     def get(self, request, *args, **kwargs):
-
         if request.user.is_authenticated:
-            return redirect('/home')
+            return redirect("/home")
 
         form = CreateUserForm()
 
         return render(
             request,
-            template_name='authentication/login.html',
+            template_name="authentication/login.html",
             context={
                 "loginform": form,
-            }
+            },
         )
 
     def post(self, request, *args, **kwargs):
-
         form = CreateUserForm()
 
-        email = request.POST.get('email')
-        password = request.POST.get('password1')
+        email = request.POST.get("email")
+        password = request.POST.get("password1")
 
         user = authenticate(request, username=email, password=password)
 
         if user:
             login(request, user)
-            return redirect('/home')
+            return redirect("/home")
         else:
             error_message = "Email or Password is Incorrect"
 
         return render(
             request,
-            template_name='authentication/login.html',
+            template_name="authentication/login.html",
             context={
                 "error_message": error_message,
                 "loginform": form,
-            }
+            },
         )
 
 
 class LogoutView(TemplateView):
-
     def get(self, request, *args, **kwargs):
-
         logout(request)
-        return redirect('/login')
+        return redirect("/login")
 
 
 class HomeView(TemplateView):
-
     def get(self, request, *args, **kwargs):
 
         if not request.user.is_authenticated:
-            return redirect('/login')
+            return redirect("/login")
 
-        if request.user.type == 'SELLER':
+        # if request.user.has_perms(['product.add_product', 'product.change_product', 'product.view_product', 'product.delete_product']):
+        if request.user.groups.filter(name='seller').exists():
             electrical_appliances = Product.objects.filter(
                 user=request.user,
-                category__name__iexact='Electrical Appliances'
-                )[:5]
+                category__name__iexact="Electrical Appliances",
+            )[:5]
             clothes = Product.objects.filter(
-                user=request.user,
-                category__name__iexact='clothes'
-                )[:5]
+                user=request.user, category__name__iexact="clothes"
+            )[:5]
             footwares = Product.objects.filter(
-                user=request.user,
-                category__name__iexact='footwares'
-                )[:5]
+                user=request.user, category__name__iexact="footwares"
+            )[:5]
             home_appliances = Product.objects.filter(
-                user=request.user,
-                category__name__iexact='home appliances'
-                )[:5]
+                user=request.user, category__name__iexact="home appliances"
+            )[:5]
         else:
             electrical_appliances = Product.objects.filter(
-                category__name__iexact='Electrical Appliances'
-                )[:5]
-            clothes = Product.objects.filter(
-                category__name__iexact='clothes'
-                )[:5]
+                category__name__iexact="Electrical Appliances"
+            )[:5]
+            clothes = Product.objects.filter(category__name__iexact="clothes")[
+                :5
+            ]
             footwares = Product.objects.filter(
-                category__name__iexact='footwares'
-                )[:5]
+                category__name__iexact="footwares"
+            )[:5]
             home_appliances = Product.objects.filter(
-                category__name__iexact='home appliances'
-                )[:5]
+                category__name__iexact="home appliances"
+            )[:5]
 
         return render(
             request,
-            template_name='home.html',
+            template_name="home.html",
             context={
                 "electrical_appliances": electrical_appliances,
                 "clothes": clothes,
                 "footwares": footwares,
                 "home_appliances": home_appliances,
                 "user_type": request.user.type,
-            }
+            },
         )
 
 
 class DefaultView(TemplateView):
-
     def get(self, request, *args, **kwargs):
-
         if request.user.is_authenticated:
-            return redirect('/home')
+            return redirect("/home")
         else:
-            return redirect('/login')
+            return redirect("/login")
--- seller/templatetags/custom_tag.py
+++ seller/templatetags/custom_tag.py
@@ -1,8 +1,15 @@
 from django import template
 from product.models import Product
+from django.contrib.auth.models import Group
 register = template.Library()
 
 
 @register.simple_tag
 def count_of_products():
     return Product.objects.count()
+
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    group = Group.objects.filter(name=group_name).first()
+    return True if group in user.groups.all() else False
